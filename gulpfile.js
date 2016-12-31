7d7f6a857c23146765e19dc9d098d7a903a71999
Brian Harwell <brianharwell@gmail.com>
2016-12-31 09:51:08 -0500

    Added files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+
+var gulp = require('gulp');
+var mjml = require('gulp-mjml');
+var runSequence = require('run-sequence');
+var del = require('del');
+
+
+gulp.task('clean', function() {
+    return del([
+        '*.html'
+    ]);
+});
+
+gulp.task('compile-mjml', function () {
+    gulp.src('*.mjml')
+        .pipe(mjml())
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('done', function() {
+});
+
+gulp.task('default', function() {
+    runSequence('clean', 'compile-mjml', 'done');
+});
